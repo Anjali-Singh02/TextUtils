4d5a0a156222336e4d42d87af6e00029f9db9033
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-03-13 13:34:15 +0530

    updated read and clear buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,10 @@ import React from 'react'
 import PropTypes  from 'prop-types'
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg bg-secondary
+    ">
     <div className="container-fluid">
-      <a className="navbar-brand" href="/">
+      <a className="navbar-brand text-white" href="/">
     {props.title}
       </a>
       <button
@@ -21,12 +22,12 @@ export default function Navbar(props) {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">
+            <a className="nav-link active text-white" aria-current="page" href="/">
               Home
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/">
+            <a className="nav-link text-white" href="/">
               About
             </a>
           </li>
@@ -38,7 +39,7 @@ export default function Navbar(props) {
             placeholder="Search"
             aria-label="Search"
           />
-          <button className="btn btn-outline-success" type="submit">
+          <button className="btn btn-dark text-white" type="submit">
             Search
           </button>
         </form>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,46 +2,76 @@ import React, { useState } from 'react';
 
 export default function TextForm(props) {
 const handleUpOnClick = () => {
-  // console.log("onClick");
+  
   let newText = text.toUpperCase();
   setText(newText);
   
 }
-const handleLowOnClick = () => {
-  // console.log("onClick");
+const handleLowOnClick = () => { 
   let newText = text.toLowerCase();
-  setText(newText);
+  setText(newText); 
+}
+
+
+
+function capitalizeText() {
+  // Capitalize the first letter of the text
+  let capitalizedtext = text.charAt(0).toUpperCase() + text.slice(1);
   
+  // Capitalize the first letter after a period
+  capitalizedtext = capitalizedtext.replace(/(\.|\?|\!)\s*(\w)/g, function(match, p1, p2) {
+    return p1 + ' ' + p2.toUpperCase();
+  });
+ setText(capitalizedtext);
 }
-const handleCapsOnClick = () => {
-  // console.log("onClick");
-  let newText = text.charAt(0).toUpperCase() + text.slice(1);
-  setText(newText);
-}
+
+
 const capitalizeFirstLetter = () => {
    let newText = text.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
    setText(newText);
+   
   }
+  const handleClear = () => {
+   let newText = '';
+   setText(newText);
+   
+  }
+  const handleRead = () => {
+    let newText = new SpeechSynthesisUtterance();
+    newText.text = text;
+    window.speechSynthesis.speak(newText);
+  }
+
   
 //***************** TEXT BOX FUNCTION ******************
   const handleOnChange = (event) => {
     // console.log("on change")
     setText(event.target.value)
   }
-  const [text, setText] = useState("Enter text here")
+  const [text, setText] = useState("")
   // setText("new text")
   return (
     <div>
-        <div className="container mb-3 my-5">
-  <label htmlFor="exampleFormControlTextarea1" className="form-label fw-bold fs-5 my- 3">{props.label}</label>
-  <textarea className="form-control border-4 my-3" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="7" />
+        <div className="container ">
+  <label htmlFor="text" className="form-label fw-bold fs-5 my- 3">{props.label}</label>
+  <textarea className="form-control border-4 my-3" value={text} placeHolder= "Enter Text Here..."onChange={handleOnChange} id="text" rows="7" />
   <div className="buttons">  
-  <button type="button" onClick={handleUpOnClick} className="btn btn-primary mx-3">Convert to UpperCase</button>
-  <button type="button" onClick={handleLowOnClick} className="btn btn-info text-white">Convert to LowerCase</button>
-  <button type="button" onClick={handleCapsOnClick} className="btn btn-secondary mx-2 text-white ">First letter Caps</button>
-  <button type="button" onClick={capitalizeFirstLetter} className="btn btn-secondary mx-2 text-white ">Convert in Title Form</button>
+  <button type="button" onClick={handleUpOnClick} className="btn btn-primary mx-1">Convert to UpperCase</button>
+  <button type="button" onClick={handleLowOnClick} className="btn btn-info mx-1 text-white">Convert to LowerCase</button>
+  <button type="button" onClick={capitalizeText} className="btn btn-secondary mx-1 text-white ">First letter Caps</button>
+  <button type="button" onClick={capitalizeFirstLetter} className="btn btn-secondary mx-1 text-white ">Convert in Title Form</button>
+  <button type="button" onClick={handleRead} className="btn btn-secondary mx-1 text-white ">Read Text</button>
+  <button type="button" onClick={handleClear} className="btn btn-secondary mx-1 text-white ">Clear Text</button>
   </div>
 </div>
+
+    <div className='container my-3'>
+      <h2>Your Text Summary</h2>
+      <p className='fw-bold'>{text.split(" ").length - 1} word {text.length} characters</p>
+      <p>{0.008*text.split(" ").length}</p>
+      <h3>Preview</h3>
+      <p>{text}</p>
+    </div>
     </div>
   )
 }
